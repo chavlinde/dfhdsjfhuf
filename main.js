567ebefc7deeb4e4e24106c7b236f686a9bed009
Bianka <142433043+chavlinde@users.noreply.github.com>
2024-09-23 11:43:22 -0300

    v

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ function soma(num1,num2){
     return num1+num2;
 }
 console.log (soma(1,2));
-function areaQuadao(lado)
-return lado**2
+function areaQuadao(lado){
+
+    return lado**2
+}
 console.log (areaQuadao(6));
