4b90599bd5e9bdee4b7a3c141b844b567063b16c
Bianka <142433043+chavlinde@users.noreply.github.com>
2024-09-30 11:20:59 -0300

    mbn

--- main.js
+++ main.js
@@ -1,21 +1,22 @@
-let nomeUsuario='bianka,hikari';
-alert(nomeUsuario)
-function soma(num1,num2){
-    return num1+num2;
-}
-console.log (soma(1,2));
-function areaQuadao(lado){
+// let nomeUsuario='bianka,hikari';
+// alert(nomeUsuario)
+// function soma(num1,num2){
+//     return num1+num2;
+// }
+// console.log (soma(1,2));
+// function areaQuadao(lado){
 
-    return lado**2
-}
-console.log (areaQuadao(6));
-let areaTriangulo= function(base,altura){
-    return (base*altura)/2
-}
-console.log (areaTriangulo);
+//     return lado**2
+// }
+// console.log (areaQuadao(6));
+// let areaTriangulo= function(base,altura){
+//     return (base*altura)/2
+// }
+// console.log (areaTriangulo);
 
-let numero= parseInt (prompt('num1.num2,num3'));
-document,Write (numero); 
-function media(){
-    return( .)
-}
+// let numero= parseInt (prompt('num1.num2,num3'));
+// document,Write (numero); 
+// function media(){
+//     return( .)
+// }
+let estados= ['Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Espírito Santo', 'Goiás', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins', 'Distrito Federal']
