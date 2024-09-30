2e02edca1de8a115bdb1c21043cc89ac473c65b4
Bianka <142433043+chavlinde@users.noreply.github.com>
2024-09-30 12:03:33 -0300

    hgvn

--- main.js
+++ main.js
@@ -19,11 +19,16 @@
 // function media(){
 //     return( .)
 // }
-let estados= ['Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Espírito Santo', 'Goiás', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins', 'Distrito Federal'];
+// let estados= ['Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Espírito Santo', 'Goiás', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins', 'Distrito Federal'];
 
-let i=0;
-while(i<estados.longht.){ 
-    console.log(i);
-    console.log(estados[i]);
-    i=i++;
+// let i=0;
+// while(i<estados.longht){ 
+//     console.log(i);
+//     console.log(estados[i]);
+//     i=i++;
+// }
+
+for (let i = 0; i estados < array.length; i++) {
+    console.log(estados[i]) = array[i];
+    
 }
